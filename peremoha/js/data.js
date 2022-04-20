d1819aa0d54a3d3945fff1a7de39a8dbb7960eb5
Igo Karpov <igorok.karpov@gmail.com>
2022-04-20 18:11:41 +0200

    updated data

--- peremoha/js/data.js
+++ peremoha/js/data.js
@@ -1,71 +1,72 @@
+//https://www.mil.gov.ua/news/2022/04/20/vtrati-rosijskih-okupantiv-stanovlyat-priblizno-20-900-osib-znishheno-majzhe-2100-bojovih-bronovanih-mashin-voroga-%E2%80%93-generalnij-shtab-zs-ukraini/
 let war = {
     "data": {
-        "tank": {
-            "total": 2840,
-            "killed": 753,
-            "divider": 100
-        },
         "soldier": {
             "total": 200000,
-            "killed": 20000 + 40000,
+            "killed": 20900 + 40000,
             "divider": 1000
+        },
+        "tank": {
+            "total": 2840,
+            "killed": 815,
+            "divider": 100
         },
         "bbm": {
             "total": "10228",
-            "killed": "1968",
+            "killed": "2087",
             "divider": 1000
+        },
+        "art": {
+            "total": 0,
+            "killed": 391,
+            "divider": 10
+        },
+        "rszv": {
+            "total": 0,
+            "killed": 136,
+            "divider": 10
+        },
+        "ppo": {
+            "total": 620,
+            "killed": 67,
+            "divider": 10
         },
         "jet": {
             "total": "300",
-            "killed": "160",
+            "killed": "171",
             "divider": 10
+        },
+        "helicopter": {
+            "total": "1052",
+            "killed": "150",
+            "divider": 10
+        },
+        "car": {
+            "total": "0",
+            "killed": "1504",
+            "divider": 100
+        },
+        "ship": {
+            "total": "20",
+            "killed": "8",
+            "divider": 1
         },
         "oil": {
             "total": "0",
             "killed": "76",
             "divider": 10
         },
-        "helicopter": {
-            "total": "1052",
-            "killed": "144",
+        "bpla": {
+            "total": 0,
+            "killed": 165,
             "divider": 10
-        },
-        "ship": {
-            "total": "20",
-            "killed": "7",
-            "divider": 1
-        },
-        "car": {
-            "total": "0",
-            "killed": "1437",
-            "divider": 100
         },
         "zvr": {
             "total": 640,
             "killed": 300,
             "divider": 10
-        },
-        "bpla": {
-            "total": 0,
-            "killed": 134,
-            "divider": 10
-        },
-        "rszv": {
-            "total": 0,
-            "killed": 122,
-            "divider": 10
-        },
-        "art": {
-            "total": 0,
-            "killed": 366,
-            "divider": 10
-        },
-        "ppo": {
-            "total": 620,
-            "killed": 64,
-            "divider": 10
         }
     },
     "dateStart": '2022-02-24',
-    "lastUpdate": '2022-04-19'
+    "lastUpdate": '2022-04-20'
 };
