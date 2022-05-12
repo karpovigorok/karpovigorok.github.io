a17e6daef2abcde955cb89d42efd16b86f4fbd2d
Igor Karpov <igor.karpov@kstt.ua>
2022-05-12 12:20:29 +0200

    update 2022-05-12

--- peremoha/js/data.js
+++ peremoha/js/data.js
@@ -1,54 +1,54 @@
-//https://www.mil.gov.ua/news/2022/05/04/vid-pochatku-povnomasshtabnoi-vijni-proti-ukraini-rosiya-vtratila-uzhe-2610-bojovih-bronovanih-mashin-znishheno-81-zasib-ppo-voroga-%E2%80%92-generalnij-shtab-zs-ukraini/
+//https://www.mil.gov.ua/news/2022/05/12/za-odinadczyat-tizhniv-povnomasshtabnoi-zbrojnoi-agresii-vtrati-rosijskih-okupantiv-stanovlyat-26650-osib-znishheno-13-korabliv-i-kateriv-voroga-%E2%80%93-genshtab-zs-ukraini/
 let war = {
     "data": {
         "soldier": {
             "total": 200000,
-            "killed": 24800 + 50000,
+            "killed": 26650 + 50000,
             "divider": 1000
         },
         "tank": {
             "total": 2840,
-            "killed": 1092,
+            "killed": 1195,
             "divider": 100
         },
         "bbm": {
             "total": "10228",
-            "killed": "2653",
+            "killed": "2873",
             "divider": 1000
         },
         "art": {
             "total": 0,
-            "killed": 507,
+            "killed": 534,
             "divider": 10
         },
         "rszv": {
             "total": 0,
-            "killed": 164,
+            "killed": 191,
             "divider": 10
         },
         "ppo": {
             "total": 620,
-            "killed": 82,
+            "killed": 87,
             "divider": 10
         },
         "jet": {
             "total": "300",
-            "killed": "194",
+            "killed": "199",
             "divider": 10
         },
         "helicopter": {
             "total": "1052",
-            "killed": "155",
+            "killed": "161",
             "divider": 10
         },
         "car": {
             "total": "0",
-            "killed": "1981",
+            "killed": "2019",
             "divider": 100
         },
         "ship": {
             "total": "20",
-            "killed": "10",
+            "killed": "13",
             "divider": 1
         },
         "oil": {
@@ -58,11 +58,11 @@ let war = {
         },
         "bpla": {
             "total": 0,
-            "killed": 315,
+            "killed": 398,
             "divider": 10
         },
         "zvr": {
-            "total": 640,
+            "total": 593,
             "killed": 300,
             "divider": 10
         }
